86e6335d329202b7fa292e78999a73147e3716e8
patrick cremin <pcremin@us.ibm.com>
2017-03-21 13:47:24 -0500

    first query

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,34 @@
+"use strict";
+
+var DiscoveryV1 = require( 'watson-developer-cloud/discovery/v1' );
+
+function getDiscoveryCredentials()
+{
+	var vcapServices = JSON.parse(process.env.VCAP_SERVICES);
 	
-/*
- * GET home page.
- */
+	// Pattern match to find the first instance of a Discovery service in
+	// VCAP_SERVICES. If you know your service key, you can access the
+	// service credentials directly by using the vcapServices object.
+	for (var vcapService in vcapServices) 
+	{
+		if (vcapService.match(/discovery/i)) 
+		{
+		        return {
+		        	username: vcapServices[vcapService][0].credentials.username,
+				password: vcapServices[vcapService][0].credentials.password
+			};
+		}
+	}
+}
+
+
+var discoveryCredentials = getDiscoveryCredentials();
+
+var discovery = new DiscoveryV1( {
+    username: discoveryCredentials.username,
+    password: discoveryCredentials.password,
+    version_date: DiscoveryV1.VERSION_DATE_2016_12_15
+} );
 
 exports.index = function(req, res){
   res.render('index.html', { title: 'Cloudant Boiler Plate' });
